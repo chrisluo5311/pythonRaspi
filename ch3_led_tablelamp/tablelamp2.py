4de4ffbfe849fa649c3aee2772679cec7d8ccf6d
Chris Luo <chrislo5311@gmail.com>
2023-02-19 22:48:00 +0800

    update tablelamp2 error

--- ch3_led_tablelamp/tablelamp2.py
+++ ch3_led_tablelamp/tablelamp2.py
@@ -10,7 +10,15 @@ print('按鈕測試開始')
 led = LED(17)
 button = Button(18)
 
-button.when_pressed = led.on
-button.when_released = led.off
+
+def onButtonPressed():
+    led.toggle()  # 如果是亮著的就關閉，如果是關燈狀態則開啟
+    if led.is_lit:
+        print("Led turned on >>>")
+    else:
+        print("Led turned off <<<")
+
+
+button.when_pressed = onButtonPressed
 
 pause()
